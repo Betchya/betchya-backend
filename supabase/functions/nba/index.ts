59b6706af5a0a1a84ecfbac2b6db82198f641336
Irunius <zacattack51196@gmail.com>
2025-07-10 17:00:31 -0400

    Feature: Add Hono Web Framework and Routing (#8)
    * refactored the update-nba-teams Edge Function to use the Controller-Service-DAO layered architecture for easier readability, maintainability, extensibility, testing, etc.
    
    * update SupabaseNbaDbDao to use an Enum for the valid schemas that can be passed to it when instantiated, constraining to valid schemas like 'nba' to connect to and query against. Update all layers to appropriately throw Errors, cleanup logging, and have Service and DAO exceptions propagate to the Controller to be handled.
    
    * update config.toml so Supabase will use Deno 2 instead of 1. Add Hono Routing Framework and RouterBuilder helper class that takes a base path, creates a Hono instance, and exposes an addRoute method that takes a path and route handler.
    
    * uncomment DAO invocation that was commented for Controller Layer testing purposes
    
    * fix relative pathing due to the update-nba-teams edge function being renamed to nba
    
    * fix wrong imports leftover from rebasing
    
    * update deno.lock version from 5 to 4 since the supabase edge runtime supports a lower minor.patch version of Deno compared to my local Deno version
    
    * update config.toml for the nba Edge Function to have the import_map property target the import_map.json file rather than deno.json since imports don't live there with an import_map being present.
    
    * refactor SupabaseNbaDbDAO to be generic, supporting any sport type, that is specified at time of creating the DAO instance via a schema argument that is of SupabaseSchemaType enum value to connect to when querying the DB.
    
    * small tweak to generic usage in SupabaseDbDAO

---
+++ supabase/functions/nba/index.ts
@@ -0,0 +1,38 @@
+import { Context } from "hono";
+import { RouterBuilder, SupportedHttpMethod } from "../shared/RouterBuilder.ts";
+import { SupabaseDbNbaService } from "./service/SupabaseNbaService.ts";
+import { NbaSportsDataDAO } from "./dao/NbaSportsDataDAO.ts";
+import { SupabaseDbDAO, SupabaseSchemaType } from "./dao/SupabaseDbDAO.ts";
+
+// Dependency Injection for Services and DAOs needed by the current Edge Function for handling NBA related request
+// Note: this index.ts file is responsible for the Controller Layer
+const sportsDataIoDAO = new NbaSportsDataDAO();
+const nbaDAO = new SupabaseDbDAO(SupabaseSchemaType.NBA); // Use the enum value for the schema
+const supabaseNbaService = new SupabaseDbNbaService(sportsDataIoDAO, nbaDAO);
+
+const routerBuilder = new RouterBuilder("/nba");
+
+/**
+ * Resource Path: /nba/teams
+ * Route/Endpoint to upsert NBA teams from Sportsdata.io to our in Supabase nba.teams DB Table.
+ */
+routerBuilder.addRoute(
+  SupportedHttpMethod.POST,
+  "/teams", 
+  async (context: Context) => {
+    try {
+      const successMessage = await supabaseNbaService.syncNbaTeamData();
+      return context.json({message: successMessage}, { status: 200 });
+    } catch (error: Error | unknown) {
+      if (error instanceof Error) {
+        return context.json({message: `Failed to update NBA teams: ${error.message}`}, { status: 500 });
+      } else {
+        return context.json({message: "Failed to update NBA teams: Unknown error"}, { status: 500 });
+      }
+      }
+  }
+);
+
+//TODO add other NBA endpoints for handling players, games, etc. as needed
+
+Deno.serve(routerBuilder.getApp().fetch);
--- supabase/functions/nba/service/SupabaseNbaService.ts
+++ supabase/functions/nba/service/SupabaseNbaService.ts
@@ -1,21 +1,22 @@
-import { NbaSupabaseDbDAO } from "../dao/NbaSupabaseDbDAO.ts";
+import { SupabaseDbDAO } from "../dao/SupabaseDbDAO.ts";
 import { NbaSportsDataDAO } from "../dao/NbaSportsDataDAO.ts";
 
 import { mapNbaTeamToDBRecord } from "../entity/NbaTeamRecord.ts";
 import * as TeamUtil from "../ro/SportsDataTeamRO.ts";
 
 import { NbaTeamRecord } from "../entity/NbaTeamRecord.ts";
+import { SupabaseSchemaType } from "../dao/SupabaseDbDAO.ts";
 
 class SupabaseDbNbaService {
   private sportsDataDAO: NbaSportsDataDAO;
-  private supabaseDbDao: NbaSupabaseDbDAO;
+  private supabaseDbDao: SupabaseDbDAO<SupabaseSchemaType.NBA>;
 
-  constructor(sportsDataDAO: NbaSportsDataDAO, supabaseDbDao: NbaSupabaseDbDAO) {
+  constructor(sportsDataDAO: NbaSportsDataDAO, supabaseDbDao: SupabaseDbDAO<SupabaseSchemaType.NBA>) {
     this.sportsDataDAO = sportsDataDAO;
     this.supabaseDbDao = supabaseDbDao;
   }
 
-  syncNbaTeamData = async (tableName: string, conflictKey: string) => {
+  syncNbaTeamData = async () => {
     const nbaAllTeamsResponse = await this.sportsDataDAO.getAllTeams();
     if (!Array.isArray(nbaAllTeamsResponse) || nbaAllTeamsResponse.length === 0) {
         throw new Error('Invalid API response: expected array of teams');
@@ -26,7 +27,7 @@ class SupabaseDbNbaService {
       throw new Error(`Invalid API Response: No valid data found in NBA All Teams Sports Data.`);
     }
 
-    const { error } = await this.supabaseDbDao.upsertRecords<NbaTeamRecord>(validatedRecords, tableName, conflictKey);
+    const { error } = await this.supabaseDbDao.upsertRecords<NbaTeamRecord>(validatedRecords, "teams", 'teamid');
     if (error) {
       const errorMsg = error?.message ? error.message : JSON.stringify(error);
       throw new Error(`Database error: ${errorMsg}`);
