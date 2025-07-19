2a28d966fc6e6fa194c727e27d5da8f6ba5ea7d1
Zac Sanford <zacattack51196@gmail.com>
2025-07-19 17:40:29 -0400

    update SUPABASE_SERVICE_ROLE_KEY usage in our development workflow to be the SUPABASE_ANON_KEY instead since that's what will actually be used from our Edge Function -> Supabase

--- supabase/functions/tests/integration/supabase-upsert-nba-teams.test.ts
+++ supabase/functions/tests/integration/supabase-upsert-nba-teams.test.ts
@@ -1,9 +1,10 @@
 import { createClient } from "supabase";
+import * as EnvironmentVariables from "../../shared/EnvironmentVariables.ts";
 
 // Replace with your local or remote Supabase API URL and service role key as needed
 const supabase = createClient(
-  "http://127.0.0.1:54321",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU"
+  EnvironmentVariables.SUPABASE_URL!,
+  EnvironmentVariables.SUPABASE_ANON_KEY!
 );
 
 // Example upsert data, adjust as needed for your schema
